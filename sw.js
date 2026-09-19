5b4b542ac572ca931e213f6de546fba0f614b75b
Minosse00 <semper2100@gmail.com>
2026-09-19 23:06:11 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,41 @@
-const CACHE_NAME = 'gps-camera-v14';
+const CACHE_NAME = 'gestione-ferie-v1';
 const urlsToCache = [
   './index.html',
   './manifest.json',
-  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
-  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js'
+  './GestioneFerie.jpg'
 ];
 
-self.addEventListener('install', event => {
+// Installazione del Service Worker e caching delle risorse
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-self.addEventListener('fetch', event => {
+// Intercettazione delle richieste per il funzionamento offline
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Attivazione e pulizia vecchie cache
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
