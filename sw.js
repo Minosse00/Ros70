f5bbed8d92fe978ca67a6a0526c132b3947fa0a7
Minosse00 <semper2100@gmail.com>
2026-09-20 10:37:54 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,41 +1,20 @@
-const CACHE_NAME = 'gestione-ferie-v1';
-const urlsToCache = [
-  './index.html',
-  './manifest.json',
-  './GestioneFerie.jpg'
-];
-
-// Installazione del Service Worker e caching delle risorse
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(urlsToCache);
-      })
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('spese-auto-store').then((cache) => {
+      return cache.addAll([
+        'index.html',
+        'manifest.json',
+        'icon-192.png',
+        'icon-512.png'
+      ]);
+    })
   );
 });
 
-// Intercettazione delle richieste per il funzionamento offline
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        return response || fetch(event.request);
-      })
-  );
-});
-
-// Attivazione e pulizia vecchie cache
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
     })
   );
 });
